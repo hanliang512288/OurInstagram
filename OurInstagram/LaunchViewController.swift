14ac762a182718755396782fdea0c096d0251f39
hanliang512288 <hanliang512288@gmail.com>
2015-09-24 21:25:43 +1000

    initialLaunchView

---
+++ OurInstagram/LaunchViewController.swift
@@ -0,0 +1,63 @@
+//
+//  LaunchViewController.swift
+//  OurInstagram
+//
+//  Created by LarryHan on 24/09/2015.
+//  Copyright (c) 2015 LarryHan. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+import SwiftyJSON
+
+class LaunchViewController: UIViewController {
+
+    let token = "2203590801.aabf771.701252ebb0f4425cbc8231c41a0e5732"
+    var userJson:JSON = nil
+    
+    @IBOutlet weak var userPic: UIImageView!
+    
+    @IBOutlet weak var userName: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        loadUserJson()
+        
+    }
+    
+    func loadUserJson(){
+        let userReqUrl = "https://api.instagram.com/v1/users/self/?access_token=\(token)"
+        Alamofire.request(.GET,userReqUrl).responseJSON{
+            (_,_,json,error) in
+
+            var jsonObj = JSON(json!)
+            let data = jsonObj["data"]
+            let name = data["username"].stringValue
+            let pictureAdd = data["profile_picture"].stringValue
+            
+            self.userName.text = name
+            
+            if let picUrl = NSURL(string: pictureAdd){
+                if let data = NSData(contentsOfURL:picUrl){
+                    self.userPic.contentMode=UIViewContentMode.ScaleAspectFill
+                    self.userPic.image=UIImage(data:data)
+
+        
+        }
+    }
+}
+}
+    
+    
+    
+    
+    
+    
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+}
